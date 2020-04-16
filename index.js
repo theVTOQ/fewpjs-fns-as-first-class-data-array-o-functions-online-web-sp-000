9ec079bb1266295d92e240adb01c7e2459f202d0
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-16 22:52:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashD
 
 function exerciseDog(dogName, dogBreed){
   for(let i = 0; i < routine.length; i++) {
-    routine[i]();
+    routine[i](dogName, dogBreed);
   }
 }
 
