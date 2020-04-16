68f254b502e3c5015dad8fbfd8e718e5f8e01652
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-16 22:51:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+const wakeDog = function wakeDog(name, breed){
+  const phrase = createPhrase("Wake", name, breed);
+  console.log(phrase);
+  return phrase;
+}
+
+const leashDog = function leashDog(name, breed){
+  const phrase = createPhrase("Leash", name, breed);
+  console.log(phrase);
+  return phrase;
+}
+
+const walkToPark = function walkToPark(name, breed){
+  const phrase = createPhrase("Walk to the park with", name, breed);
+  console.log(phrase);
+  return phrase;
+}
+
+const throwFrisbee = function throwFrisbee(name, breed){
+  const phrase = createPhrase("Throw the frisbee for", name, breed);
+  console.log(phrase);
+  return phrase;
+}
+
+const walkHome = function walkHome(name, breed){
+  const phrase = createPhrase("Walk home with", name, breed);
+  console.log(phrase);
+  return phrase;
+}
+
+const unleashDog = function unleashDog(name, breed){
+  const phrase = createPhrase("Unleash", name, breed);
+  console.log(phrase);
+  return phrase;
+}
+
+function exerciseDog(dogName, dogBreed){
+  const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+  for(let i = 0; i < routine.length; i++) {
+    routine[i]();
+  }
+}
+
+function createPhrase(verb, name, breed){
+  return `${verb} ${name} the ${breed}`;
+}
